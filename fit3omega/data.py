2f03f5d2eb7f18f5c97da95fe578456cc298ab2c
Ara G <ghukasa@mcmaster.ca>
2021-01-21 23:27:52 -0500

    satisfied with Data

--- fit3omega/data.py
+++ fit3omega/data.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import NamedTuple
 
 
-class Voltage(NamedTuple):
+class ACVoltageReadings(NamedTuple):
     x: np.array
     y: np.array
 
@@ -15,10 +15,9 @@ class Data:
         if error_csv:
             self._error = pd.read_csv(error_csv, header="infer")
         else:
-            error_csv = data_csv.split(".")[:-1] + ".error.csv"
+            error_csv = '.'.join(data_csv.split('.')[:-1]) + ".error.csv"
             try:
                 self._error = pd.read_csv(error_csv, header="infer")
-                self.error = self._error.copy()
             except FileNotFoundError:
                 self._error = None
 
@@ -34,10 +33,10 @@ class Data:
         self._start = None
         self._end = None
 
-    def drop(self, row_index):
-        self._data.drop(row_index, axis=0)
-        if self._error:
-            self._error.drop(row_index, axis=0)
+    def drop_row(self, row_index):
+        self._data = self._data.drop(row_index, axis=0)
+        if self._error is not None:
+            self._error = self._error.drop(row_index, axis=0)
 
     @property
     def data(self):
@@ -45,13 +44,13 @@ class Data:
 
     @property
     def error(self):
-        if not self._error:
+        if self._error is None:
             raise ValueError("no error data has been initialized")
         return self._error[self._start:self._end]
 
     @error.setter
     def error(self, error_csv):
-        if self._error:
+        if self._error is not None:
             raise ValueError("error data already set")
 
         e = pd.read_csv(error_csv, header="infer")
@@ -66,12 +65,36 @@ class Data:
 
     @property
     def V(self):
-        return Voltage(self.data['Vs_1w'].values, self.data['Vs_1w_o'].values)
+        return ACVoltageReadings(self.data['Vs_1w'].values,
+                                 self.data['Vs_1w_o'].values)
 
     @property
-    def V3w(self):
-        return Voltage(self.data['Vs_3w'].values, self.data['Vs_3w_o'].values)
+    def V3(self):
+        return ACVoltageReadings(self.data['Vs_3w'].values,
+                                 self.data['Vs_3w_o'].values)
 
     @property
     def Vsh(self):
-        return Voltage(self.data['Vsh_1w'].values, self.data['Vsh_1w_o'].values)
+        return ACVoltageReadings(self.data['Vsh_1w'].values,
+                                 self.data['Vsh_1w_o'].values)
+
+    @property
+    def dV(self):
+        if self._error is None:
+            raise ValueError("no error data has been initialized")
+        return ACVoltageReadings(self.error['dVs_1w'].values,
+                                 self.error['dVs_1w_o'].values)
+
+    @property
+    def dV3(self):
+        if self._error is None:
+            raise ValueError("no error data has been initialized")
+        return ACVoltageReadings(self.error['dVs_3w'].values,
+                                 self.error['dVs_3w_o'].values)
+
+    @property
+    def dVsh(self):
+        if self._error is None:
+            raise ValueError("no error data has been initialized")
+        return ACVoltageReadings(self.error['dVsh_1w'].values,
+                                 self.error['dVsh_1w_o'].values)
